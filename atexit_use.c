847e227b3d140d96191acf254b51f5022b098d90
chenlin <chenlin2844@hotmail.com>
2017-06-30 09:35:23 +0800

    init file "atexit_use"
    Signed-off-by: chenlin <chenlin2844@hotmail.com>

---
+++ atexit_use.c
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: atexit_use.c
+	> Author: Chen Lin
+	> Mail: chenlin2844@hotmail.com
+	> Created Time: 2017年06月30日 星期五 09时31分25秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+static void my_exit1(void);
+static void my_exit2(void);
